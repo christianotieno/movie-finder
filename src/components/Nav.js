401cb59d5812fead32c0f3eb832b1a653c5ecde1
christianotieno <oduorchristian@gmail.com>
2021-06-18 22:15:16 +0300

    fix home link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Nav = () => (
   <nav className="deep-orange">
     <div className="nav-wrapper container">
-      <a href="www.google.com" className="brand-logo">Movie finder</a>
+      <a href="https://my-react-movie-finder.netlify.app/" className="brand-logo">Movie finder</a>
     </div>
   </nav>
 );
